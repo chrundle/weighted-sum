77c277065989de0324fb6da62efe2187131aec73
chrundle <jdiffen1@gmail.com>
2019-03-11 13:16:16 -0400

    First commit for simple class

---
+++ weighted_sum.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+x = np.array([1,2,3,4,5])
+w = np.array([1,2,-1,3,2])
+
+# Initialize sum
+sum = 0 
+
+# Perform weighted summation
+for i in range(len(x)):
+  sum = sum + w[i] * x[i] 
+
+# Print summation
+print "sum = ", sum
+
+# Instead use dot product function
+print "sum with dot product = ", np.dot(w,x)
